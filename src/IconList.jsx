290e3ccac984d9d51621b0f3e39a5ace6573141f
Amber <110812972+Msambere@users.noreply.github.com>
2025-01-12 15:23:44 -0600

    Update new tech skills

--- src/IconList.jsx
+++ src/IconList.jsx
@@ -10,7 +10,11 @@ const IconList = [
   { id: uuid4(), src: '/icons-64/python.png', alt: 'python icon' },
   { id: uuid4(), src: '/icons-64/react.png', alt: 'react icon' },
   { id: uuid4(), src: '/icons-64/Rlogo.png', alt: 'R icon' },
-  { id: uuid4(), src: '/icons-64/tableau-icon.png', alt: 'tableau icon' }
+  { id: uuid4(), src: '/icons-64/tableau-icon.png', alt: 'tableau icon' },
+  { id: uuid4(), src: '/icons-64/icons8-java.svg', alt: 'java icon' },
+  { id: uuid4(), src: '/icons-64/icons8-spring-boot.svg', alt: 'spring boot icon' },
+  { id: uuid4(), src: '/icons-64/icons8-postgres.svg', alt: 'postgres icon' },
+  { id: uuid4(), src: '/icons-64/flask.svg', alt: 'flask icon' }
 ];
 
 export { IconList };
